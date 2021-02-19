5e68efedc2acf749bfe73fbe313f31eee3eafd0d
bengisu taşkın <taskinbengisu0@gmail.com>
2021-02-19 15:34:36 +0300

    KampÖdevi

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKeys = keys;
+            keys = new TKey[keys.Length + 1];
+
+            TValue[] tempValues = values;
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[1] = tempKeys[i];
+                values[1] = tempValues[i];
+
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+        public int count
+        {
+            get { return keys.Length; }
+        }
+        public void List()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("KEY : "+keys[i]+ " value :" + values[i]);
+            }
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> name1 = new MyDictionary<int, string>();
+            name1.Add(1, "Bengisu");
+            name1.Add(2, "Nursu");
+            name1.Add(3, "Ahmet");
+            name1.Add(4, "Mehmet");
+            name1.Add(5, "Fatma");
+            name1.Add(6, "Mustafa");
+            Console.WriteLine("MyDictionary : "+name1.count );
+
+            
+
+            MyDictionary<int, string> name2 = new MyDictionary<int, string>();
+            name2.Add(1, "Kerem");
+            name2.Add(2, "Ayşe");
+            name2.Add(3, "Dicle");
+            name2.Add(4, "Mahmut");
+            name2.Add(5, "Efe");
+            name2.Add(6, "Mert");
+            Console.WriteLine("MyDictionary : " + name2.count);
+           
+            
+
+
+
+        }
+    }
+}
